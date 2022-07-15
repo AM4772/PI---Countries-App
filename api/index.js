984b1bc0d552da8cf0ecc14ece0d3a8e853665d8
AM4772 <moro_bramanti@hotmail.com>
2022-07-15 14:04:50 -0300

    12th commit

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { dbLoad } = require('./src/controllers/DbloaderController.js');
 const { conn } = require('./src/db.js');
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || '3001';     
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
