9b0e061b7db0f6a6258171d9b735b2658e12fc6d
AM4772 <moro_bramanti@hotmail.com>
2022-07-15 12:41:31 -0300

    12th commit

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { dbLoad } = require('./src/controllers/DbloaderController.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => { // for deployment with Heroku: change 3001 to process.env.PORT
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     await dbLoad();
