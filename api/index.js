93375cf0c3ca6db82b34eb36f93212782b7777a1
AM4772 <moro_bramanti@hotmail.com>
2022-07-15 16:06:05 -0300

    13th commit

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 const PORT = process.env.PORT || '3001';     
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, async () => { // for deployment with Heroku: change 3001 to process.env.PORT
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     await dbLoad();
