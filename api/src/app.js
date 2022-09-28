b04f02745a17dfc13e93f0ced81bb91eecc69dfd
AM4772 <moro_bramanti@hotmail.com>
2022-09-27 23:43:55 -0300

    upgraded version

--- api/src/app.js
+++ api/src/app.js
@@ -1,33 +1,40 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
 
-require('./db.js');
+require("./db.js");
 
 const server = express();
-const cors = require('cors');
+const cors = require("cors");
 
-server.name = 'API';
+server.name = "API";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
   // Access-Control-Allow-Origin ---> update to match the domain (URL) you will make the request from
-  res.header('Access-Control-Allow-Origin', '*'); // http://localhost:3000
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://pi-countries-app.vercel.app/"
+  ); // http://localhost:3000
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-server.use('/', routes);
+server.use("/", routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
